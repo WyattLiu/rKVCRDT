5f91251d134c2a1dd63bcd707e81e827f4bbb39b
yunhaom94e <yunhaom94@outlook.com>
2022-04-01 02:04:31 +0000

    added a quick test

---
+++ RACClient/src/quik_test.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+from client import *
+from startservers import *
+import random
+
+addresses = start_server(3)
+
+def split_ipport(address):
+
+    res = address.split(":")
+
+    return res[0], int(res[1])
+
+def connect(addresses):
+    res = []
+    for i in range(len(addresses)):
+        adddress = addresses[i]
+        ip, port = split_ipport(adddress)
+        s = Server(ip, port)
+        
+        if (s.connect() == 0):
+            print("Connection to " + adddress + " failed, exiting")
+            exit()
+
+        res.append(s)
+
+
+    print("Connection complete")
+    return res
+
+
+def PNC_Test():
+    
+    pnc = []
+    connections = connect(addresses)
+
+    for i in range(len(addresses)):
+        pnc.append(PNCounter(connections[i]))
+
+    result = 0
+
+    pnc[0].set("test", result)
+    time.sleep(1)
+
+    for i in range(100):
+        server_id = i % 3
+        num = random.randint(-10, 10)
+        if (num < 0):
+            pnc[server_id].dec("test", abs(num))
+        else:
+            pnc[server_id].inc("test", num)
+
+        result += num
+
+    time.sleep(1)
+    try:
+        for i in range(len(addresses)):
+            assert(int(pnc[server_id].get("test")[1][0]) == result)
+    finally:
+        print("Result server: " + str(pnc[i].get("test")) + " local: " + str(result))
+        stop_server()
+        
+    
+
+
+if __name__ == "__main__":
+    PNC_Test()
