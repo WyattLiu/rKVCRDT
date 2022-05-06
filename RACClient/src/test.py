bb267ef5a8f5998947988513e6148cc3960fbc10
Zong <wyatt_liu@hotmail.com>
2022-05-05 23:21:10 -0400

    file

---
+++ RACClient/src/test.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+from multibench import *
+
+# Insturction:
+# 1. Make a copy of this file and rename to run_multi_bench.py
+# 2. Change SERVER_LIST to remote servers with RAC server
+# 3. see jsons below to define a workload
+
+SERVER_LIST = ["192.168.41.237", 
+                "192.168.41.100",
+                "192.168.41.76",
+                "192.168.41.161",
+                "192.168.41.248",
+                "192.168.41.31",
+                "192.168.41.254",
+                "192.168.41.85",
+                "192.168.41.86",
+                "192.168.41.79" ]
+
+
+if __name__ == "__main__":
+    test = {
+        "nodes_pre_server": 1,
+        "use_server": 5,
+        "client_multiplier": [100],
+
+        "typecode": "rc",
+        "total_objects": [100],
+
+        "prep_ops_pre_obj": 1000,
+        "num_reverse": 100,
+        "prep_ratio": [1, 0, 0],
+
+
+        "ops_per_object": 1000,
+        "op_ratio": [0.25, 0.25, 0.5],
+        "target_throughput": 0
+    }
+
+    run_experiment(test, "client_multiplier", "total_objects", "test", SERVER_LIST, True)
+
